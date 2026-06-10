8c2127b2e7cc8004fea3bd7570d3851b15d2cf80
dstN <github@yinside.de>
2026-06-10 15:38:38 +0200

    Empty the repo

--- src/components/tab-content/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface TabContentProps {
-  children: ReactNode;
-}
-
-export default function TabContent({ children }: TabContentProps) {
-  return <div data-lk-component="tab-content">{children} </div>;
-}
